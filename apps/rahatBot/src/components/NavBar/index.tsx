abafc073e9a45423e5b1d03d7c4f840bd661cbc8
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2024-02-23 17:54:08 +0530

    chore: conversation id updated on lang toggle

--- apps/rahatBot/src/components/NavBar/index.tsx
+++ apps/rahatBot/src/components/NavBar/index.tsx
@@ -1,21 +1,21 @@
-import { useContext, useCallback, useEffect } from 'react';
-import styles from './index.module.css';
-import YogiModiImg from '../../assets/images/yogimodi.png';
-import UPGovtLogo from '../../assets/images/up-govt-logo.png';
-import plusIcon from '../../assets/icons/plus.svg';
-import Image from 'next/image';
-import { logEvent } from 'firebase/analytics';
+import { useContext, useCallback, useEffect } from "react";
+import styles from "./index.module.css";
+import YogiModiImg from "../../assets/images/yogimodi.png";
+import UPGovtLogo from "../../assets/images/up-govt-logo.png";
+import plusIcon from "../../assets/icons/plus.svg";
+import Image from "next/image";
+import { logEvent } from "firebase/analytics";
 // import { analytics } from '../../utils/firebase';
-import { AppContext } from '../../context';
-import router from 'next/router';
-import { v4 as uuidv4 } from 'uuid';
-import { useLocalization } from '../../hooks';
-import toast from 'react-hot-toast';
-import flagsmith from 'flagsmith/isomorphic';
-import axios from 'axios';
-import React from 'react';
-import { useCookies } from 'react-cookie';
-import { Sidemenu } from '../Sidemenu';
+import { AppContext } from "../../context";
+import router from "next/router";
+import { v4 as uuidv4 } from "uuid";
+import { useLocalization } from "../../hooks";
+import toast from "react-hot-toast";
+import flagsmith from "flagsmith/isomorphic";
+import axios from "axios";
+import React from "react";
+import { useCookies } from "react-cookie";
+import { Sidemenu } from "../Sidemenu";
 
 function NavBar() {
   const t = useLocalization();
@@ -23,60 +23,66 @@ function NavBar() {
   const [cookie, setCookies] = useCookies();
 
   const [isEngActive, setIsEngActive] = React.useState(
-    localStorage.getItem('locale')
-      ? localStorage.getItem('locale') === 'en'
-      : 'en'
+    localStorage.getItem("locale")
+      ? localStorage.getItem("locale") === "en"
+      : "en"
   );
 
-  useEffect(()=>{
-    setIsEngActive(context?.locale==='en')
-  },[context?.locale])
+  useEffect(() => {
+    setIsEngActive(context?.locale === "en");
+  }, [context?.locale]);
 
   const toggleLanguage = React.useCallback(
     (newLanguage: string) => () => {
       if (newLanguage === context?.locale) return;
-      const message = context?.locale === 'hi' ? "भाषा बटन को टॉगल करने से भाषा बदल जाएगी और वर्तमान सत्र पुनः आरंभ हो जाएगा" :"Toggling the language button will change language and restart the current session"
-      if(window?.confirm(message))
-      {localStorage.setItem('locale', newLanguage);
-      context?.setLocale(newLanguage);
-      setIsEngActive((prev) => (prev === true ? false : true));}
+      const message =
+        context?.locale === "hi"
+          ? "भाषा बटन को टॉगल करने से भाषा बदल जाएगी और वर्तमान सत्र पुनः आरंभ हो जाएगा"
+          : "Toggling the language button will change language and restart the current session";
+      if (window?.confirm(message)) {
+        localStorage.setItem("locale", newLanguage);
+        context?.setLocale(newLanguage);
+        context?.setNewConversationId(uuidv4())
+        setIsEngActive((prev) => (prev === true ? false : true));
+      }
     },
     [context]
   );
 
   const newChatHandler = useCallback(() => {
     if (context?.isMsgReceiving) {
-      toast.error(`${t('error.wait_new_chat')}`);
+      toast.error(`${t("error.wait_new_chat")}`);
       return;
     }
 
-
     const newConversationId = uuidv4();
-    sessionStorage.setItem('conversationId', newConversationId);
+    sessionStorage.setItem("conversationId", newConversationId);
     if (context?.audioElement) context?.audioElement.pause();
     if (context?.setAudioPlaying) context?.setAudioPlaying(false);
     context?.setConversationId(newConversationId);
     context?.setMessages([]);
     context?.setIsMsgReceiving(false);
     context?.setLoading(false);
-    router.push('/');
-    toast.success(`${t('label.new_chat_started')}`);
+    router.push("/");
+    toast.success(`${t("label.new_chat_started")}`);
   }, [context, t]);
 
-  if (router.pathname === '/chat' && !context?.isDown) {
+  if (router.pathname === "/chat" && !context?.isDown) {
     return (
-      <div className={styles.navbar + ' ' + styles.main}>
+      <div className={styles.navbar + " " + styles.main}>
         <div
-          style={{ width: '120px', display: 'flex', alignItems: 'flex-end' }}>
+          style={{ width: "120px", display: "flex", alignItems: "flex-end" }}
+        >
           {/* <Sidemenu /> */}
           <div
             style={{
-              paddingLeft: '15px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-            }}>
+              paddingLeft: "15px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
             <Image
               src={plusIcon}
               alt=""
@@ -84,8 +90,8 @@ function NavBar() {
               height={28}
               onClick={newChatHandler}
             />
-            <p style={{ color: 'var(--primary)', fontSize: '12px' }}>
-              {t('label.new_chat')}
+            <p style={{ color: "var(--primary)", fontSize: "12px" }}>
+              {t("label.new_chat")}
             </p>
           </div>
         </div>
@@ -99,45 +105,43 @@ function NavBar() {
     );
   } else
     return (
-      <div className={styles.main} style={router.pathname === '/login' ? { height: '120px' } : {}}>
-        <div
-          className={styles.navbar} >
-{/* <Sidemenu /> */}
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div
+        className={styles.main}
+        style={router.pathname === "/login" ? { height: "120px" } : {}}
+      >
+        <div className={styles.navbar}>
+          {/* <Sidemenu /> */}
+          <div style={{ display: "flex", alignItems: "center" }}>
             <button
               id="eng"
               className={isEngActive ? styles.active : styles.btn}
-              style={{ borderRadius: '10px 0px 0px 10px' }}
-              onClick={toggleLanguage('en')}>
+              style={{ borderRadius: "10px 0px 0px 10px" }}
+              onClick={toggleLanguage("en")}
+            >
               ENG
             </button>
             <button
               id="hindi"
               className={!isEngActive ? styles.active : styles.btn}
-              style={{ borderRadius: '0px 10px 10px 0px' }}
-              onClick={toggleLanguage('hi')}>
+              style={{ borderRadius: "0px 10px 10px 0px" }}
+              onClick={toggleLanguage("hi")}
+            >
               हिंदी
             </button>
-          </div> 
+          </div>
 
-          <div
-          >
+          <div>
             <Image src={UPGovtLogo} alt="" width={60} height={60} />
-           
           </div>
           <div>
-
             <Image src={YogiModiImg} alt="" width={110} height={65} />
-
           </div>
-
         </div>
-       
-          <div
-            className={styles.title}
-            dangerouslySetInnerHTML={{ __html: t('label.title') }}>      
-            </div>
-      
+
+        <div
+          className={styles.title}
+          dangerouslySetInnerHTML={{ __html: t("label.title") }}
+        ></div>
       </div>
     );
 }
